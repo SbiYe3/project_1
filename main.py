0fd560e4930026532db30edb9e0688a63cff1e11
Oussama <air@MacBook-Air-de-Air.local>
2025-12-10 20:17:48 +0100

    realiser la fonction add numbers

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ array1 = np.array([1, 2, 3])
 array2 = np.array([4, 5, 6])
 array_sum = np.add(array1, array2)
 print(f"The sum of {array1} and {array2} is {array_sum}.")
+
+def add_numbers(a, b):
+    return a + b
+result = add_numbers(10, 20)
+print(f"The sum of 10 and 20 is {result}.")
