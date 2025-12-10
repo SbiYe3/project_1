06642606613ae3baa52d9ca5d815900578ffb658
Oussama <air@MacBook-Air-de-Air.local>
2025-12-10 19:47:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}.")
+
+import numpy as np 
+
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+array_sum = np.add(array1, array2)
+print(f"The sum of {array1} and {array2} is {array_sum}.")
